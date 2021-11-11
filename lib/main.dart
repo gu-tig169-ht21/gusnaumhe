9fe991b21f033f8f963195b21f16b7f7e00e26c0
naumburg <gusnaumhe@student.gu.se>
2021-11-11 20:42:57 +0100

    uppdatering

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,16 @@
 import 'package:flutter/material.dart';
 
+
+class NotificationSetting {
+  String title;
+  bool value;
+
+  NotificationSetting({
+    required this.title,
+    this.value = false,
+  });
+}
+//--------------------------------------------------------------------------------------------------
 void main() {
   runApp(MyApp());
 }
@@ -14,26 +25,95 @@ class MyApp extends StatelessWidget {
 }
 
 class MainView extends StatelessWidget {
+
+  final notifications = [
+      NotificationSetting(title: 'Write a book'),
+      NotificationSetting(title: 'Do homework'),
+      NotificationSetting(title: 'Tidy up room'),
+  ];
+
+  get value => null;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        // ignore: prefer_const_constructors
+        title: Text(
+          'TIG169 TODO',
+          style: const TextStyle(color: Colors.white,fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+        actions: [
+          IconButton(icon: Icon(Icons.more_vert), 
+          onPressed: () {
+            // 
+            // ----------- Denna ska flyttas!! ----------------------------
+            //
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => SecondView()));
+            //
+            //-------------------------------------------------------------
+            //
+          }),
+        ], 
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            _appBar(),
+            //_appBar(),
+            ...notifications.map(buildSingleCheckbox).toList(),
+           // _buildCheckbox(),
           ],
         ), //Column
       ), //Center
     ); //Scaffold 
   }
 
+
   Widget _appBar() {
     return Container(
       height: 53,
-      decoration: BoxDecoration(color: Colors.blue),
-      child: Center(child: Text('Title', style: TextStyle(color: Colors.white, fontSize: 24, fontWeight: FontWeight.bold),),)
+      decoration: BoxDecoration(color: Colors.blueGrey[200]),
+      child: Center(child: Text('TIG169 TODO', style: TextStyle(color: Colors.white, fontSize: 24, fontWeight: FontWeight.bold),),)
+      );
+    }
+
+  Widget buildSingleCheckbox(NotificationSetting notification) =>
+    _buildCheckbox(
+      notification: notification,
+      onClicked: () {
+        setState(() {
+          final newValue = !notification.value;
+          notification.value = newValue;
+        });
+      },
     );
+
+  Widget _buildCheckbox({
+    required NotificationSetting notification,
+    required VoidCallback onClicked, 
+  }) => ListTile(
+    onTap: onClicked,
+  leading: Checkbox(
+        value: notification.value,
+        onChanged: (value) => onClicked(),
+      ),
+      title: Text(
+        notification.title, 
+        style: TextStyle(fontSize: 20, fontWeight: FontWeight.normal)),
+    );
+
+  void setState(Null Function() param0) {} 
+}
+
+class SecondView extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Scaffold();
   }
 }
+
+
+
